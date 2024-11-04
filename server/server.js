c739478e64ab36c8a7509799af76eae15fbcde05
Sivaramakrishnan <sivaramakrishnan0327@gmail.com>
2024-11-04 13:41:01 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Enable CORS
+app.use(cors()); // Enable CORS for all origins (this can be adjusted in production)
 app.use(bodyParser.json());
 
 // Use routes
