daf1eac09c46d228fd7fc10a381f36608628f888
Sivaramakrishnan <sivaramakrishnan0327@gmail.com>
2024-11-08 11:39:16 +0530

    style and download option included

--- faculty/src/auth/connection.js
+++ faculty/src/auth/connection.js
@@ -1,5 +1,5 @@
-const baseURL = 'http://localhost:5000';
-export default baseURL;
-
-// const baseURL = 'https://kite-cgpa-backend.vercel.app';
+// const baseURL = 'http://localhost:5000';
 // export default baseURL;
+
+const baseURL = 'https://kite-cgpa-backend.vercel.app';
+export default baseURL;
